ec389c019ea6b0a9545001a051c4b81a6bb371a3
Your Name <you@example.com>
2024-12-28 21:08:34 +0300

    RestApi POst  ve Get halledildi RestEmployeeController

---
+++ Controllers/RestEmployee.cs
@@ -0,0 +1,63 @@
+﻿using BarberSaloon.Data;
+using BarberSaloon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BarberSaloon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BarberSaloon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestEmployeeController : ControllerBase
+    {
+        private readonly BarberSaloonDBContext _context;
+
+        public RestEmployeeController(BarberSaloonDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/RestEmployee
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        // POST: api/RestEmployee
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = employee.EmployeeID }, employee);
+        }
+
+        // GET: api/RestEmployee/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployeeById(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+    }
+}
